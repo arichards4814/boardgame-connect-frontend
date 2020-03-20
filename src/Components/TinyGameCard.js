440d689724c26579be2559851dd991380622f6fc
arichards4814 <arichards4814@gmail.com>
2020-03-20 14:15:31 -0400

    styled the choose games and fixed bugs

--- src/Components/TinyGameCard.js
+++ src/Components/TinyGameCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import GamesContainer from '../Containers/GamesContainer';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid';
 
 function TinyGameCard(props) {
 
@@ -9,7 +10,18 @@ function TinyGameCard(props) {
     return (
         <div>
             <Paper>
-                <Typography variant="body2">{props.name} {props.year_published}</Typography>
+                <Grid container>
+                    <Grid tile md={1}>
+                    </Grid>
+                    <Grid tile md={1}>
+                        <img src={props.thumb_url} style={{ height: 30 }} />
+                    </Grid>
+                    <Grid tile md={8}>
+                        <Typography variant="h5" component="h2">{props.name} {props.year_published}</Typography>
+                    </Grid>
+                </Grid>
+                
+                
             </Paper>
         </div>
     );
