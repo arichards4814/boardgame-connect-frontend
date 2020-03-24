1c89ecffd02cead3e9f0a4cae28a1c284d9cb860
arichards4814 <arichards4814@gmail.com>
2020-03-24 09:57:30 -0400

    added boardgame place holder for host a game

--- src/Components/HostGameView.js
+++ src/Components/HostGameView.js
@@ -11,13 +11,13 @@ export default function HostGameView(props){
         <Paper>
             <Grid container spacing={3} style={{paddingLeft: 8}}>
                 <Grid item>
-                    <img src={props.chosenGame && props.chosenGame.image_url} style={{height: 130, width: 160}}></img>
+                    <img src={props.chosenGame ? props.chosenGame.image_url : './images/boardgame_placeholder.png'} style={{height: 130, width: 160}}></img>
                 </Grid>
                 <Grid item>
-                    <Typography variant="h5">Game: {props.chosenGame && props.chosenGame.name}</Typography>
-                    <Typography variant="body1">Minimum # of Players: {props.chosenGame && props.chosenGame.min_players}</Typography>
-                    <Typography variant="body1">Maximum # of Players: {props.chosenGame && props.chosenGame.max_players}</Typography>
-                    <Typography variant="body1">Minimum Play Time: {props.chosenGame && props.chosenGame.min_playtime} minutes</Typography>
+                    <Typography variant="h4" align="left">Game: {props.chosenGame && props.chosenGame.name}</Typography>
+                    <Typography variant="body1" align="left">Minimum # of Players: {props.chosenGame && props.chosenGame.min_players}</Typography>
+                    <Typography variant="body1" align="left">Maximum # of Players: {props.chosenGame && props.chosenGame.max_players}</Typography>
+                    <Typography variant="body1" align="left">Minimum Play Time: {props.chosenGame && props.chosenGame.min_playtime} minutes</Typography>
                 </Grid>
             </Grid>
         </Paper>
