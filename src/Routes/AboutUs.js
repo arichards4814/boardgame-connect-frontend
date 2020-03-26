2fff8ad3cb62f76bd6f124daab5da1a3b76d6539
Miles Higbie <miles.higbie@gmail.com>
2020-03-26 15:34:32 -0400

    added about us route and onclick event from nav

---
+++ src/Routes/AboutUs.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import TopNav from '../Components/TopNav'
+
+    const AboutUs = (props) => {
+        return(
+            <TopNav history={props.history} />
+        )
+    }
+
+export default AboutUs
